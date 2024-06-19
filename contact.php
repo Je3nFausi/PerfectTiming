9e1edf8950e18de4b60f1e39d1c4dd75479e464f
Je3nFausi <jeanette.omondi@strathmore.edu>
2024-06-19 08:09:29 +0300

    well

--- contact.php
+++ contact.php
@@ -12,22 +12,42 @@
       </head>
       <body>
         
-      <?php include_once("template/nav.php");?>
-      <?php require_once("includes/dbconnect.php");?>
+      
+      <?php
+      include_once("template/nav.php");
+      require_once("includes/dbconnect.php");
+
+      if(isset($POST["submit"])){
+      $em = $_POST["email"];
+      $pass =  $_POST["password"];
+
+$insert = "INSERT INTO customer (email,password)
+VALUES ('$em', '$pass')";
+
+if ($conn->query($insert) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $insert . "<br>" . $conn->error;
+}
+      }
+
+$conn->close();
+      ?>
     
         </nav>
         <h1 style="text-align: center; margin-top: 50px; color: rgb(114, 18, 18);">GET FIRST HAND INFO!</h1>
         <div class="container-fluid">
-            <form class="form">
+            <form action="<?php print htmlspecialchars($_SERVER["PHP_SELF"]);?>"
+             method="post">
                 <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
                   <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
 
                 <div class="mb-3">
                   <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1">
+                  <input type="password" class="form-control" id="exampleInputPassword1" name="password">
                 </div>
 
                 <div class="mb-3 form-check">
@@ -36,7 +56,7 @@
                 </div>
 
                 <button type="submit" class="btn btn-primary">Submit</button>
-              </form>
+                </form>
         </div>
         <!-- Option 1: Bootstrap Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
